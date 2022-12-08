afdb3f136578fa810d92bdf2900ce99af15ac19b
Sammy-CK <sammycherono2696@gmail.com>
2022-12-08 15:20:38 +0300

    Completed assignment

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-let form = document.getElementById('github-form');
+let form = document.getElementById('github-form');//form to submit search
 
 let nameSearch = document.getElementById('search'); //actual search value
 
@@ -7,26 +7,27 @@ let ulUserResult = document.getElementById('user-list'); // ul for username resu
 let ulRepoResults = document.getElementById('repos-list'); // ul for repo results
 
 
+document.addEventListener('DOMContentLoaded', form.addEventListener('submit',usersSearchResult))
 
-form.addEventListener('submit',(e) => {
+
+//finds users when search is submitted 
+function usersSearchResult(e){
     e.preventDefault();
 
+//resets page before new search results are entered
     ulRepoResults.innerHTML=''
     ulUserResult.innerHTML=''
     fetch(`https://api.github.com/search/users?q=${nameSearch.value}`)
     .then(resp => resp.json())
     .then(data  => {
         
+//Incase no user is found matching submitted Username
         if(data.total_count === 0){
             alert(`No users named : ${nameSearch.value}, Try Again`)
         }
         (data.items).forEach(user => {
 
-
-
             let li = document.createElement('li')
-
-
 
             li.classList.add('userName')
 
@@ -42,47 +43,37 @@ form.addEventListener('submit',(e) => {
 
             li.appendChild(button)
 
-                button.addEventListener('click',() => { 
 
-                    ulRepoResults.innerHTML=''
-    fetch(`https://api.github.com/users/${user.login}/repos`)
-    .then(response => response.json())
-    .then(dataz => {
-        dataz.forEach(repo => {
+//shows all user's repositories when repository button is clicked
+function showAllRepo(){ 
+    ulRepoResults.innerHTML=''
 
-            let liRepo = document.createElement('li')
+fetch(`https://api.github.com/users/${user.login}/repos`)
+.then(response => response.json())
+.then(dataz => {
+dataz.forEach(repo => {
+let liRepo = document.createElement('li')
 
+liRepo.innerHTML = `
+                <h3>Repository Name: ${repo.name}</h3>
+                <p>Repository details: ${repo.description}</p>
+                <p>Click to open Repository<a href=${repo.html_url}>${repo.html_url}</a></p>`
 
-
-            liRepo.innerHTML = `
-                                <h3>Repository Name: ${repo.name}</h3>
-                                <p>Repository details: ${repo.description}</p>
-                                <p>Click to open Repository<a href=${repo.html_url}>${repo.html_url}</a></p>`
-
-            ulRepoResults.append(liRepo)
-
+ulRepoResults.append(liRepo)
 })
-        })
-
 })
+}
+//end
 
 
-
-
-
+            button.addEventListener('click', showAllRepo)
             ulUserResult.appendChild(li) 
-
-            
+       
         })
-
-
-
-        
 
     })
+//Incase of error alerts user of error
     .catch(error =>{
     alert(error.message)
     })
-
-
-})
+}
